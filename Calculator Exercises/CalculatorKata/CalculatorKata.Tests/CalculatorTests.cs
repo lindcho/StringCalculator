366b6a944827faa52eec2dad5adc0251f667d0c4
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2019-08-13 09:39:31 +0200

    refactor tests

--- Calculator Exercises/CalculatorKata/CalculatorKata.Tests/CalculatorTests.cs
+++ Calculator Exercises/CalculatorKata/CalculatorKata.Tests/CalculatorTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using NUnit.Framework.Constraints;
 using System.Linq;
 
@@ -11,10 +12,12 @@ namespace CalculatorKata.Tests
         [TestCase(null, 0)]
         public void Add_GivenEmptyString_ShouldReturnZero(string input, int expectedResult)
         {
+            //Arrange
             var sut = CreateCalculator();
-
+            //Act
             var actual = sut.Add(input);
-            Assert.That(actual, Is.EqualTo(0));
+            //Assert
+            Assert.That(actual, Is.EqualTo(expectedResult));
         }
 
         [TestCase("1", 1)]
@@ -22,9 +25,11 @@ namespace CalculatorKata.Tests
         [TestCase("143", 143)]
         public void Add_GivenValidNumber_ShouldReturnSameNumber(string input, int expectedResult)
         {
+            //Arrange
             var sut = CreateCalculator();
-
+            //Act
             var actual = sut.Add(input);
+            //Assert
             Assert.That(actual, Is.EqualTo(expectedResult));
         }
 
@@ -34,9 +39,11 @@ namespace CalculatorKata.Tests
         [TestCase("30,76,2,65,2,5", 180)]
         public void Add_GivenTwoCommaSeparatedInput_ShouldReturnInputSum(string input, int expectedResult)
         {
+            //Arrange
             var sut = CreateCalculator();
-
+            //Act
             var actual = sut.Add(input);
+            //Assert
             Assert.That(actual, Is.EqualTo(expectedResult));
         }
 
@@ -46,9 +53,11 @@ namespace CalculatorKata.Tests
         [TestCase("1\n5,4", 10)]
         public void Add_GivenNewLineDelimiterBetweenInput_ShouldReturnInputSum(string input, int expectedResult)
         {
+            //Arrange
             var sut = CreateCalculator();
-
+            //Act
             var actual = sut.Add(input);
+            //Assert
             Assert.That(actual, Is.EqualTo(expectedResult));
         }
 
@@ -58,9 +67,48 @@ namespace CalculatorKata.Tests
         [TestCase("//$\n1", 1)]
         public void Add_GivenCustomDelimiterBetweenInput_ShouldReturnInputSum(string input, int expectedResult)
         {
+            //Arrange
             var sut = CreateCalculator();
-
+            //Act
             var actual = sut.Add(input);
+            //Assert
+            Assert.That(actual, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase("-1,-2,-4,56,9", "negatives not allowed -1 -2 -4")]
+        [TestCase("1,-5,20", "negatives not allowed -5")]
+        public void Add_GivenInputWithNegativeValues_ShouldThrowError(string input, string expectedResult)
+        { 
+            //Arrange
+            var sut = CreateCalculator();
+            //Act
+            var actual = Assert.Throws<ArgumentException>(() => sut.Add(input));
+            //Assert
+            Assert.That(actual.Message, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase("//;\n4;2", 6)]
+        [TestCase("//[*][%]\n5*8%23", 36)]
+        public void Play_GivenInputWithDifferentDelimitersWithAnyLength_ShouldReturnSum(string input, int expectedResult)
+        {
+            //Arrange
+            var sut = CreateCalculator();
+            //Act
+            var actual = sut.Add(input);
+            //Assert
+            Assert.That(actual, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase("1002,100", 100)]
+        [TestCase("1000,21", 1021)]
+        [TestCase("999,1", 1000)]
+        public void Play_GivenInputWithNumbersGreaterThan1000_ShouldOnlyNumbersLessThan1000(string input, int expectedResult)
+        {
+            //Arrange
+            var sut = CreateCalculator();
+            //Act
+            var actual = sut.Add(input);
+            //Assert
             Assert.That(actual, Is.EqualTo(expectedResult));
         }
 
@@ -80,14 +128,32 @@ namespace CalculatorKata.Tests
             }
 
             var inputArray = input.Replace('\n', ',').Split(',');
+            GetDelimiters(ref inputArray);
 
-            if (inputArray[0].StartsWith("//"))
+            ValidateNumbersArePositive(inputArray);
+
+            return inputArray.Where(x=>int.Parse(x)<=1000).Sum(x => int.Parse(x));
+        }
+
+        private static void ValidateNumbersArePositive(string[] inputArray)
+        {
+            if (inputArray.Any(x => int.Parse(x) < 0))
             {
-                inputArray[1] = inputArray[1].Replace(';', ',');
-                inputArray = inputArray[1].Split(',');
+                throw new ArgumentException(
+                    $"negatives not allowed {string.Join(" ", inputArray.Where(x => int.Parse(x) < 0))}");
+            }
+        }
+
+        private static void GetDelimiters(ref string[] inputArray)
+        {
+            if (!inputArray[0].StartsWith("//")) return;
+            var customDelimiter = inputArray[0].Remove(0, 2);
+            foreach (var delimiter in customDelimiter)
+            {
+                inputArray[1] = inputArray[1].Replace(delimiter, ',');
             }
 
-            return inputArray.Sum(x => int.Parse(x));
+            inputArray = inputArray[1].Split(',');
         }
     }
 }
