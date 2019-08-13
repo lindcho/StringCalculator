a979f84a65a771035c7b8ccb2c034e58153c0c4e
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2019-08-13 08:56:20 +0200

    add custom delimiter implementation and tests

--- Calculator Exercises/CalculatorKata/CalculatorKata.Tests/CalculatorTests.cs
+++ Calculator Exercises/CalculatorKata/CalculatorKata.Tests/CalculatorTests.cs
@@ -7,49 +7,60 @@ namespace CalculatorKata.Tests
     [TestFixture]
     public class CalculatorTests
     {
-        [TestCase("",0)]
-        [TestCase(null,0)]
-        public void Add_GivenEmptyString_ShouldReturnZero(string input,int expectedResult)
+        [TestCase("", 0)]
+        [TestCase(null, 0)]
+        public void Add_GivenEmptyString_ShouldReturnZero(string input, int expectedResult)
         {
-            var sut=new Calculator();
+            var sut = new Calculator();
 
             var actual = sut.Add(input);
-            Assert.That(actual,Is.EqualTo(0));
+            Assert.That(actual, Is.EqualTo(0));
         }
 
-        [TestCase("1",1)]
-        [TestCase("20",20)]
-        [TestCase("143",143)]
-        public void Add_GivenValidNumber_ShouldReturnSameNumber(string input,int expectedResult)
+        [TestCase("1", 1)]
+        [TestCase("20", 20)]
+        [TestCase("143", 143)]
+        public void Add_GivenValidNumber_ShouldReturnSameNumber(string input, int expectedResult)
         {
-            var sut=new Calculator();
+            var sut = new Calculator();
 
             var actual = sut.Add(input);
-            Assert.That(actual,Is.EqualTo(expectedResult));
+            Assert.That(actual, Is.EqualTo(expectedResult));
         }
 
-        [TestCase("6,9",15)]
-        [TestCase("120,56",176)]
-        [TestCase("30,76",106)]
-        [TestCase("30,76,2,65,2,5",180)]
-        public void Add_GivenTwoCommaSeparatedInput_ShouldReturnInputSum(string input,int expectedResult)
+        [TestCase("6,9", 15)]
+        [TestCase("120,56", 176)]
+        [TestCase("30,76", 106)]
+        [TestCase("30,76,2,65,2,5", 180)]
+        public void Add_GivenTwoCommaSeparatedInput_ShouldReturnInputSum(string input, int expectedResult)
         {
-            var sut=new Calculator();
+            var sut = new Calculator();
 
             var actual = sut.Add(input);
-            Assert.That(actual,Is.EqualTo(expectedResult));
+            Assert.That(actual, Is.EqualTo(expectedResult));
         }
 
         [TestCase("1\n2", 3)]
         [TestCase("1\n14", 15)]
         [TestCase("1\n1,4", 6)]
         [TestCase("1\n5,4", 10)]
-        public void Add_GivenNewLineDelimiterBetweenInput_ShouldReturnInputSum(string input,int expectedResult)
+        public void Add_GivenNewLineDelimiterBetweenInput_ShouldReturnInputSum(string input, int expectedResult)
         {
-            var sut=new Calculator();
+            var sut = new Calculator();
 
             var actual = sut.Add(input);
-            Assert.That(actual,Is.EqualTo(expectedResult));
+            Assert.That(actual, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase("//;\n1;2", 3)]
+        [TestCase("//;\n43;2,3", 45)]
+        [TestCase("//;\n7;2;6;5", 20)]
+        public void Add_GivenCustomDelimiterBetweenInput_ShouldReturnInputSum(string input, int expectedResult)
+        {
+            var sut = new Calculator();
+
+            var actual = sut.Add(input);
+            Assert.That(actual, Is.EqualTo(expectedResult));
         }
     }
 
@@ -63,8 +74,14 @@ namespace CalculatorKata.Tests
             }
 
             var inputArray = input.Replace('\n', ',').Split(',');
-            
-            return inputArray.Sum(x=>int.Parse(x));
+
+            if (inputArray[0].StartsWith("//"))
+            {
+                inputArray[1] = inputArray[1].Replace(';', ',');
+                inputArray = inputArray[1].Split(',');
+            }
+
+            return inputArray.Sum(x => int.Parse(x));
         }
     }
 }
