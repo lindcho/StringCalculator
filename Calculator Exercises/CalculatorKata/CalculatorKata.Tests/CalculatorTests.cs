fa33f86a69e2bf1338eee782b485cbcaf6704bc5
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2019-08-13 08:27:52 +0200

    create project for 13 august

---
+++ Calculator Exercises/CalculatorKata/CalculatorKata.Tests/CalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace CalculatorKata.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [TestCase("",0)]
+        [TestCase(null,0)]
+        public void Add_GivenEmptyString_ShouldReturnZero(string input,int expectedResult)
+        {
+            var sut=new Calculator();
+
+            var actual = sut.Add(input);
+            Assert.That(actual,Is.EqualTo(0));
+        }
+
+        [TestCase("1",1)]
+        [TestCase("20",20)]
+        [TestCase("143",143)]
+        public void Add_GivenValidNumber_ShouldReturnSameNumber(string input,int expectedResult)
+        {
+            var sut=new Calculator();
+
+            var actual = sut.Add(input);
+            Assert.That(actual,Is.EqualTo(expectedResult));
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+            return int.Parse(input);
+        }
+    }
+}
