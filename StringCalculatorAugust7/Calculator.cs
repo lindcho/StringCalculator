2a2965dbc714a6fcc10ea9130f2bec76e7773df1
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2019-08-16 09:09:16 +0200

    refactor: improve tests

--- StringCalculatorAugust7/Calculator.cs
+++ StringCalculatorAugust7/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace StringCalculatorAugust7
@@ -11,31 +12,56 @@ namespace StringCalculatorAugust7
             if (string.IsNullOrEmpty(input))
                 return 0;
 
-            var numberStringArray = input.Replace('\n', delimiter).Split(delimiter);
-            GetCustomDelimiter(ref numberStringArray);
-            ValidateNegatives(numberStringArray);
-            var numberArray = numberStringArray.Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse).ToArray();
-            return numberArray.Where(x => x > 0 && x <= 1000).Sum(x => x);
+            var numbers = input.Replace('\n', delimiter).Split(delimiter);
+
+            GetCustomDelimiter(ref numbers);
+
+            ValidateNegatives(numbers);
+
+            var numberArray = numbers.Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse).ToArray();
+
+            return FilterNumbersLessThan1000(numberArray).Sum(x => x);
         }
 
-        public static void ValidateNegatives(string[] numberArray)
+        private static IEnumerable<int> FilterNumbersLessThan1000(int[] numberArray)
         {
-            if (!numberArray.Any(x => int.Parse(x) < 0)) return;
-            throw new Exception($"negatives not allowed {string.Join(" ", numberArray.Where(x => int.Parse(x) < 0))}");
+            return numberArray.Where(x => x < 1000);
+        }
+
+        private static void ValidateNegatives(string[] numberArray)
+        {
+            var negativeNumbers = numberArray.Where(x => int.Parse(x) < 0).ToList();
+            if (!negativeNumbers.Any()) return;
+            throw new Exception($"negatives not allowed {string.Join(" ", negativeNumbers)}");
         }
 
         private static void GetCustomDelimiter(ref string[] numberStringArray)
         {
-            if (numberStringArray[0].StartsWith("//"))
+            if (StartsWith(numberStringArray))
             {
-                var customDelimiters = numberStringArray[0].Remove(0, 2);
+                var customDelimiters = DelimiterPart(numberStringArray).Remove(0, 2);
 
                 foreach (var delimiter in customDelimiters)
                 {
-                    numberStringArray[1] = numberStringArray[1].Replace(delimiter, ',');
+                    numberStringArray[1] = NumbersPart(numberStringArray).Replace(delimiter, ',').Trim("[".ToCharArray()).Trim();
                 }
-                numberStringArray = numberStringArray[1].Split(',');
+                numberStringArray = NumbersPart(numberStringArray).Split(',');
             }
+        }
+
+        private static string NumbersPart(string[] numberStringArray)
+        {
+            return numberStringArray.Last();
+        }
+
+        private static string DelimiterPart(string[] numberStringArray)
+        {
+            return numberStringArray.First();
+        }
+
+        private static bool StartsWith(string[] numberStringArray)
+        {
+            return numberStringArray[0].StartsWith("//");
         }
     }
 }
